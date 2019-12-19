b34536f6bc80061795496ab0086b5d96b57cd2c8
Josh Holinaty <holinaty@gmail.com>
2019-12-19 10:10:46 -0500

    minimal dialog box

--- Player.gd
+++ Player.gd
@@ -1,8 +1,6 @@
 extends Node2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var speed = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,21 +9,26 @@ func _ready():
 func _process(delta):
 	if Input.is_action_pressed("right"):
 		move("right")
+		if get_node("Sprite").flip_h:
+			get_node("Sprite").flip_h = false
 	if Input.is_action_pressed("left"):
 		move("left")
+		if !get_node("Sprite").flip_h:
+			get_node("Sprite").flip_h = true
 	if Input.is_action_pressed("up"):
 		move("up")
 	if Input.is_action_pressed("down"):
 		move("down")
+	
 
 func move(word):
 	match word:
 		"right":
-			position.x += 1
+			position.x += floor(1 * speed);
 		"left":
-			position.x -= 1
+			position.x -= floor(1 * speed);
 		"up":
-			position.y -= 1
+			position.y -= floor(1 * speed);
 		"down":
-			position.y += 1
+			position.y += floor(1 * speed);
 
---
+++ dialogBoxl.gd
@@ -0,0 +1,42 @@
+# dialoGbox.gd
+extends RichTextLabel
+
+var dialog = [
+	"Where's my bone?",
+	"Oh, there it is!"
+]
+var page = 0;
+
+var showText;
+onready var bone = preload("res://Bone.tscn")
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	set_process_input(true)
+	showText = false;
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _input(event):
+	if event is InputEventKey and event.scancode == KEY_SPACE:
+		if showText:
+			if visible_characters > get_total_character_count():
+				if page == 0 and page < dialog.size()-1:
+					page += 1
+					bbcode_text = dialog[page]
+					visible_characters = 0
+					var b = bone.instance()
+					b.position.x = 16
+					b.position.y = 32
+					get_tree().get_root().add_child(b)
+					
+		else:
+			showText = !showText
+			bbcode_text = dialog[page]
+			visible_characters = 0
+
+func _on_Timer_timeout():
+	
+	visible_characters = visible_characters+1;
