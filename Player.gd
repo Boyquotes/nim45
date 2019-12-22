08c3f8de014fbffa76649d86d1856b9b2f715c32
Josh Holinaty <holinaty@gmail.com>
2019-12-22 13:27:09 -0500

    Click to move doggy.

--- Player.gd
+++ Player.gd
@@ -1,12 +1,21 @@
+# Player.gd
 extends Node2D
 
-var speed = 1
+var speed = 4
+var tween
+var dog
+var property = "transform/position"
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	tween = get_node("Tween")
+	dog = get_node("Sprite")
 
 func _process(delta):
+	handleInput()
+
+func handleInput():
+	mouseClick()
+	# KeyBoardControls
 	if Input.is_action_pressed("right"):
 		move("right")
 		if get_node("Sprite").flip_h:
@@ -19,7 +28,12 @@ func _process(delta):
 		move("up")
 	if Input.is_action_pressed("down"):
 		move("down")
+
+func mouseClick():
+	if Input.is_mouse_button_pressed(1):
+		move_ball_to(get_local_mouse_position())
 	
+
 
 func move(word):
 	match word:
@@ -31,4 +45,23 @@ func move(word):
 			position.y -= floor(1 * speed);
 		"down":
 			position.y += floor(1 * speed);
+
+
+func move_ball_to(target_pos):
+	var start = dog.position
+	var end = target_pos
+	var distance = start.distance_to(end)
+	var direction = start - end;
+	if direction.x < 0:
+		dog.flip_h = false
+	else:
+		dog.flip_h = true
+	var time = distance / 40
+	
+	if time <= 0: return
+	
+	tween.interpolate_property(dog, "position",
+        start, end, time,
+        Tween.TRANS_CUBIC, Tween.EASE_IN_OUT)
+	tween.start()
 
